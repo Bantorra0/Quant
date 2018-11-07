5007337f6c3d376cb704318108f48ac4a0810b2e
Jie Wu <u5835210@anu.edu.au>
2018-11-08 07:45:15 +0800

    Update.

--- scribble.py
+++ scribble.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 x = -np.arange(100)
 y = np.random.rand(100)
-plt.plot(x,y)
+line = plt.plot(x,y)
 plt.xticks([0,40,60,80])
+plt.legend([line],["line"])
 plt.show()
