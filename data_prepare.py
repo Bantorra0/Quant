be514794e8b0924abee5d2bcbceabe3272f68108
Jie Wu <jiewufd@outlook.com>
2018-12-29 18:58:08 +0800

    Modify future mean calculating in data_prepare.

--- data_prepare.py
+++ data_prepare.py
@@ -291,18 +291,23 @@ def proc_stck_d(df_stck_d, stock_pool=None,targets=None):
             pred_period = t["period"]
             if t["fun"]=="min":
                 df_target = rolling(t["fun"], pred_period, move(-1, df, cols=t["col"]))
-                df_targets_list.append(df_target)
             elif t["fun"]=="max":
-                df_target = rolling(t["fun"],pred_period - 1, move(-2, df, cols=t["col"]))
-                df_targets_list.append(df_target)
+                # df_target = rolling(t["fun"],pred_period - 1, move(-2, df, cols=t["col"]))
+                df_target = rolling(t["fun"], pred_period, move(-1, df, cols=t["col"]))
             elif t["fun"]=="mean":
-                p1 = (pred_period - 1) // 3
-                p2 = p1
-                p3 = pred_period - 1 - p1 - p2
-                df_period_mean1 = rolling(t["fun"], p1, move(-2, df, t["col"]))
-                df_period_mean2 = rolling(t["fun"], p2, move(-2 - p1, df, t["col"]))
-                df_period_mean3 = rolling(t["fun"], p3, move(-2 - p1 - p2, df, t["col"]))
-                df_targets_list.extend([df_period_mean1,df_period_mean2,df_period_mean3])
+                # df_target = rolling(t["fun"], pred_period - 1, move(-2, df, cols=t["col"]))
+                df_target = rolling(t["fun"], pred_period, move(-1, df, cols=t["col"]))
+
+                # p1 = (pred_period - 1) // 3
+                # p2 = p1
+                # p3 = pred_period - 1 - p1 - p2
+                # df_period_mean1 = rolling(t["fun"], p1, move(-2, df, t["col"]))
+                # df_period_mean2 = rolling(t["fun"], p2, move(-2 - p1, df, t["col"]))
+                # df_period_mean3 = rolling(t["fun"], p3, move(-2 - p1 - p2, df, t["col"]))
+                # df_targets_list.extend([df_period_mean1,df_period_mean2,df_period_mean3])
+            else:
+                raise ValueError("Fun type {} is not supported!".format(t["fun"]))
+            df_targets_list.append(df_target)
 
         df_move_list = [move(i, df, cols_move) for i in move_mv_list]
         df_move_change_list = [change_rate(df_move,df[cols_move])
