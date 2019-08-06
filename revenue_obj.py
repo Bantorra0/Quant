48f50a664a6c4657d19976416e0aff2fa0b8df49
Jie Wu <jiewufd@outlook.com>
2019-08-06 18:19:55 +0800

    Fix a bug in custom_r_obj_wrapper2.

--- revenue_obj.py
+++ revenue_obj.py
@@ -115,11 +115,22 @@ def hess_relu(x,k=100):
 #     return custom_r_obj
 
 
-def custom_r_obj_wrapper(k=100):
+def custom_r_obj_wrapper(k=1):
     def custom_r_obj(y_true,y_pred):
         p0 = y_pred
         l = -y_true
         grad_p = grad_relu(p0,k)
+        grad = grad_p * l
+        hess = np.ones(shape=grad.size)
+        return grad,hess
+    return custom_r_obj
+
+
+def custom_r_obj_wrapper2(k=1):
+    def custom_r_obj(y_true,y_pred):
+        p0 = 1/(1+np.exp(-k*y_pred))
+        l = -y_true
+        grad_p = k*p0*(1-p0)
         grad = grad_p * l
         hess = np.ones(shape=grad.size)
         return grad,hess
